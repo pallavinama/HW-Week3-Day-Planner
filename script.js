b8c526616e27bd86b42b59db7dc5ad0934265b99
pallavi <pallavi_nama@yahoo.com>
2020-03-12 21:27:54 -0400

    Code to display current date when page loads - Updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Testing script file");
+$(document).ready( function(){
+    console.log("Testing Document Ready");
+})
+var NowMoment = moment();
+var eDisplayMoment = document.getElementById('currentDay');
+eDisplayMoment.innerHTML = NowMoment.format('dddd, MMMM Do YYYY');
+console.log("testing");
